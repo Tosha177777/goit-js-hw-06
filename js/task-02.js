734651da23d462ede738efecf379f75c2a21aab5
Tosha177777 <slavkoanton4@gmail.com>
2023-09-11 14:40:39 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,19 +8,19 @@ const ingredients = [
 ];
 
 const listEl = document.querySelector("#ingredients");
-const fragment = document.createDocumentFragment();
-for (let i = 0; i < ingredients.length; i++) {
-  let element = document.createElement("li");
-  element.textContent = ingredients[i];
-  element.classList.add("item");
-  fragment.appendChild(element);
-}
-listEl.appendChild(fragment);
+// const fragment = document.createDocumentFragment();
+// for (let i = 0; i < ingredients.length; i++) {
+//   let element = document.createElement("li");
+//   element.textContent = ingredients[i];
+//   element.classList.add("item");
+//   fragment.appendChild(element);
+// }
+// listEl.appendChild(fragment);
 
-// const elements = ingredients.map((element) => {
-//   const liEl = document.createElement("li");
-//   liEl.textContent = element;
-//   liEl.classList.add("item");
-//   return liEl;
-// });
-// listEl.append(...elements);
+const elements = ingredients.map((element) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = element;
+  liEl.classList.add("item");
+  return liEl;
+});
+listEl.append(...elements);
